1c7e98767f62478e85c3b794932833d8d390fcd5
Kevin <67015555+VienThanh12@users.noreply.github.com>
2024-04-01 13:36:00 +0300

    Create Exercise3.py

---
+++ AutomationTechnologyMath/Probability/Exercise3.py
@@ -0,0 +1,19 @@
+K = 100000
+
+n = 10
+t = 50
+
+ans = 0
+for k in range(K):
+  check = np.zeros(n + 1)
+  coupons = randint(1, n + 1, t)
+  for i in range(t):
+    check[coupons[i]] += 1
+  ok = True
+  for i in range(1, n + 1):
+    if(check[i] == 0):
+      ok = False
+  if(ok == True):
+    ans += 1
+  
+print(ans / K)
