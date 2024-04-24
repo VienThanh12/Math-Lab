76fc67d11a9eb788278c94a2bc56c15a28dbed3e
Kevin <67015555+VienThanh12@users.noreply.github.com>
2024-04-24 18:50:21 +0300

    100% Accuracy

--- AutomationTechnologyMath/DFT/Exercise1.py
+++ AutomationTechnologyMath/DFT/Exercise1.py
@@ -1,72 +1,78 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Given
+# Define parameters
+A = 3  # Amplitude
+f = 22  # Frequency in Hz
+phi = 0 # Phase angle in radians
+N = 20  # Number of samples
+fs = 100  # Sampling frequency in Hz
 
-A_ft = 3
-# First case
-#f_ft = 22
-# Second case
-f_ft = 20
-phi_ft = 0
-N_ft = 20
-
-fs= 100 #sampling frequency
-
-N = 20
-#Solve
-t=np.linspace(0,T,1000)
-
-#First case
-# y = np.array([3, 0.5, -2.8, -1.5, 2.2, 2.4, -1.2, -2.9, 0.2, 3, 0.9, -2.7, -1.9, 1.9, 2.6, -0.9, -3, -0.2, 2.9, 1.3])
-# Second case
-y = np.array([3, 0.9, -2.4, -2.4, 0.9, 3, 0.9, -2.4, -2.4, 0.9, 3, 0.9, -2.4, -2.4, 0.9, 3, 0.9, -2.4, -2.4, 0.9])
-M = int(N / 2)
 n = np.arange(0, N)
+ts = n / fs
 
-a0 = 1 / N * np.sum(y)
+f0 = fs / N
+T = 1 / f0
+t = np.linspace(0, T, 200000)
+
+signal = A * np.cos(2 * pi * f * ts + phi)
+
+t_2 = np.linspace(0, T, 20000)
+signal_2 = A * np.cos(2 * np.pi * f * t_2 + phi)
+
+print(signal)
+
+######### 
+
+M = int(N / 2)
+
+a0 = 1 / N * np.sum(signal)
 a = np.zeros(M)
 b = np.zeros(M)
 
 from numpy import cos, sin, pi
 for k in range(1,M):
-    a[k-1]=2/N*np.sum(y*cos(2*pi*k*n/N))
-    b[k-1]=2/N*np.sum(y*sin(2*pi*k*n/N))
+    a[k-1]=2/N*np.sum(signal*cos(2*pi*k*n/N))
+    b[k-1]=2/N*np.sum(signal*sin(2*pi*k*n/N))
 
-a[M-1]=1/N*np.sum(y*cos(2*pi*M*n/N))
-b[M-1]=1/N*np.sum(y*sin(2*pi*M*n/N))
+a[M-1]=1/N*np.sum(signal*cos(2*pi*M*n/N))
+b[M-1]=1/N*np.sum(signal*sin(2*pi*M*n/N))
 
-A=np.sqrt(a**2+b**2) #amplitudes
-phi=np.arctan2(-b,a) #phase angles
+A_dft = np.sqrt(a**2+b**2) #amplitudes
+phi_dft = np.arctan2(-b,a) #phase angles
 
-f0=fs/N #basic frequency
-f=np.arange(f0,(M+1)*f0,f0) #frequencies
-
-T=1/f0
-ts=n/fs
-print(ts)
-
-t=np.linspace(0,T,1000)
+f_dft=np.arange(f0,(M+1)*f0,f0) #frequencies
 
 plt.figure(figsize=(7,4))
 
 yt=a0
 for k in range(M):
-    yt=yt+A[k]*cos(2*pi*f[k]*t+phi[k])
+    yt=yt+A_dft[k]*cos(2*pi*f_dft[k]*t+phi_dft[k])
 
+# Plot the signal f(t)
 plt.subplot(211)
-plt.plot(t, A_ft * cos(2 * pi * f_ft * t + phi_ft),lw=2,label='f(t)')
 plt.plot(t, yt,'orange', label = 'y(t)',lw=2)
-plt.plot(ts,y,'ro')
-plt.grid()
+
+plt.plot(t_2, signal_2, label='f(t)')
+
 plt.legend(fontsize = 14)
 plt.xlabel('time',fontsize=14)
-plt.title('A = ' + str(A_ft) + ', f = '+ str(f_ft) + ', $\phi$ = '+str(phi_ft) + ', N = ' + str(N_ft)+ ', $f_s$ = '+ str(fs))
+plt.title('A = ' + str(A) + ', f = '+ str(f) + ', $\phi$ = '+str(phi) + ', N = ' + str(N)+ ', $f_s$ = '+ str(fs))
+
+plt.plot(ts, signal, 'ro')
+
+# Plot the samples
+
+plt.grid(True)
+
+plt.tight_layout()
 
 plt.subplot(212)
-plt.bar(np.hstack((0,f)),np.hstack((a0,A)),edgecolor='k',zorder=2)
+plt.bar(np.hstack((0,f_dft)),np.hstack((a0,A_dft)),edgecolor='k',zorder=2)
 plt.grid()
-plt.xticks(np.hstack((0,f)))
+plt.xticks(np.hstack((0,f_dft)))
 plt.xlabel("frequency")
 plt.ylabel("amplitudes")
+plt.show()
+
 plt.show()
